f0100666668509f855740f03c9ecceba3ed88183
ShockwaveComputer <dovershockwave@gmail.com>
2025-10-07 20:46:40 -0400

    push up changes during Governor's Cup

--- src/main/java/frc/robot/commands/CoralAutoOn.java
+++ src/main/java/frc/robot/commands/CoralAutoOn.java
@@ -15,7 +15,8 @@ public class CoralAutoOn extends Command {
 
     @Override
     public void initialize() {
-        m_coralSubsystem.setRollerSpeed(0.5);
+        // set speed from 0.5 to 0 to not sends it out
+        m_coralSubsystem.setRollerSpeed(0.0);
         m_timer.reset(); // Reset the timer to zero
         m_timer.start(); // Start counting
         System.out.println("Starting Coral command...");
